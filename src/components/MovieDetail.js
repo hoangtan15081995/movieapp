c07483b932a05053661169623691e5b8e0a74652
hoangtan15081995 <98004746+hoangtan15081995@users.noreply.github.com>
2022-04-11 11:11:44 +0700

    update

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -3,34 +3,48 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { Box, CardActionArea } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { URL } from "../data/config";
-
+import "./DetailPage.css";
 function MovieDetail({ movie }) {
-  const navigate = useNavigate();
   return (
-    <Card>
-      <CardActionArea onClick={() => navigate(`/detail/${movie.id}`)}>
-        <CardMedia
-          component="img"
-          height="700"
-          image={`${URL}${movie.poster_path}`}
-          alt={movie.title}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {movie.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {movie.vote_average}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {movie.release_date}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      <div className="bigBox">
+        <div
+          className="detailMovie"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${
+              movie.backdrop_path || movie.poster_path
+            })`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="detail">
+          <CardMedia
+            component="img"
+            width="300"
+            height="700"
+            image={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h3" component="div">
+              {movie.title}
+            </Typography>
+            <Typography variant="h5" mb={1}>
+              VOTE_AVERAGE: {movie.vote_average}
+            </Typography>
+            <Typography variant="h5" mb={5}>
+              RELEASE_DATE: {movie.release_date}
+            </Typography>
+            <Typography variant="h5" mb={2}>
+              OVERVIEW: {movie.overview}
+            </Typography>
+          </CardContent>
+        </div>
+      </div>
+    </>
   );
 }
 export default MovieDetail;
